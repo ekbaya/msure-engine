33f2e5f261e747f878503e1ae855ad5ec130e5af
ekbaya <Eliasbaya1223@gmail.com>
2022-05-30 22:26:25 +0300

    initial Commit

---
+++ app/Services/AspinEngine.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+/**
+ * Class AspinEngine.
+ */
+class AspinEngine
+{
+    private function getAccessToken($identifier)
+    {
+        $token = Cache::get($identifier . '_aspin_engine_token');
+        if (is_null($token)) {
+            $basic_token = base64_encode(config('app.aspinengine.client_id') . ':' . config('app.aspinengine.client_secret'));
+            $username = config('app.aspinengine.username');
+            $password = config('app.aspinengine.password');
+            $response = Http::withHeaders([
+                'Authorization' => 'Basic ' . $basic_token
+            ])
+                ->withoutVerifying()
+                ->post('https://engine.staging.aspin-inclusivity.com/oauth/token?grant_type=password&scope=all&username=' . $username . '&password=' . $password);
+            if ($response->successful()) {
+                Log::info('==ASPIN TOKEN==' . $response);
+                $token = $response->json('access_token');
+                Cache::put($identifier . '_aspin_engine_token', $token, $response->json('expires_in') - 10);
+            } else {
+                Log::info('==ERROR==' . $response);
+                $response->throw();
+            }
+        }
+
+
+        return $token;
+    }
+
+    //**************************CUSTOMER MANAGEMENT****************************************/
+
+    //register customer(we will pass user model through observers)
+    public function registerCustomer(): mixed
+    {
+        $identifier = config('app.aspinengine.identifier'); //Identifier for Msure
+        $url = config('app.aspinengine.base_url') . '/customers';
+        $payload = [
+            "full_name" => "Elias Baya",
+            "msisdn" => "00271603773356",
+            "first_name" => "Elias",
+            "partner_guid" => "demo",
+            "display_language" => "en",
+            "national_id" => "123456789",
+            "beneficiary_msisdn" => "002250700000001",
+            "beneficiary_name" => "John Bar",
+            "date_of_birth" => "2000-12-03",
+            "external_identifier" => "external_identifier",
+            "account_number" => "account_number",
+            "account_type" => "account_type",
+            "branch_code" => "branch_code",
+            "registration_channel" => "ApiClient"
+        ];
+        $response = Http::withHeaders(['Authorization' => 'Bearer ' . $this->getAccessToken($identifier)])
+            ->withoutVerifying()
+            ->post($url, $payload);
+        Log::info($response->body());
+        return $response->json();
+    }
+
+    //get customer status{We are going to pass user phone}
+    public function getCustomerStatus(): mixed
+    {
+        $phone = "00271603773356";
+        $partner = config('app.aspinengine.partner_guid');
+        $identifier = config('app.aspinengine.identifier'); //Identifier for Msure
+        $url = config('app.aspinengine.base_url') . '/customers/' . $phone . '/status?partner=' . $partner;
+        $response = Http::withHeaders(['Authorization' => 'Bearer ' . $this->getAccessToken($identifier)])
+            ->withoutVerifying()
+            ->get($url);
+        Log::info($response->body());
+        return $response->json();
+    }
+
+    //update Customer
+    public function updateCustomer(): mixed
+    {
+        $identifier = config('app.aspinengine.identifier'); //Identifier for Msure
+        $url = config('app.aspinengine.base_url') . '/customers';
+        $payload = [
+            "guid" => "0066ed36a490469488721b11155b5ec2",
+            "full_name" => "Bar",
+            "msisdn" => "00271603773356",
+            "first_name" => "Foo",
+            "partner_guid" => "demo",
+            "display_language" => "fr",
+            "national_id" => "national_id",
+            "beneficiary_msisdn" => "002250700000001",
+            "beneficiary_name" => "beneficiary name",
+            "date_of_birth" => "2000-12-03"
+        ];
+        $response = Http::withHeaders(['Authorization' => 'Bearer ' . $this->getAccessToken($identifier)])
+            ->withoutVerifying()
+            ->put($url, $payload);
+        Log::info($response->body());
+        return $response->json();
+    }
+
+    //serach Customers
+    public function searchCustomers(): mixed
+    {
+        $phone = "00271603773356";
+        $name = "Foo";
+        $partner = config('app.aspinengine.partner_guid');;
+        $size = "10";
+        $page = "0";
+        $identifier = config('app.aspinengine.identifier'); //Identi//Identifier for Msure
+        $url = config('app.aspinengine.base_url') . '/customers/find?msisdn=' . $phone . '&name=' . $name . '&partner=' . $partner . '&size=' . $size . '&page=' . $page;
+        $response = Http::withHeaders(['Authorization' => 'Bearer ' . $this->getAccessToken($identifier)])
+            ->withoutVerifying()
+            ->get($url);
+        Log::info($response->body());
+        return $response->json();
+    }
+
+    //**************************PRODUCTS****************************************/
+    //get products
+    public function getAllProducts(): mixed
+    {
+        $partner = config('app.aspinengine.partner_guid');
+        $identifier = config('app.aspinengine.identifier'); //Identifier for Msure
+        $url = config('app.aspinengine.base_url') . '/products?partner=' . $partner;
+        $response = Http::withHeaders(['Authorization' => 'Bearer ' . $this->getAccessToken($identifier)])
+            ->withoutVerifying()
+            ->get($url);
+        Log::info($response->body());
+        return $response->json();
+    }
+}
