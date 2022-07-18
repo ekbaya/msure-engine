845d94205b9cf6fd60319ec68a4c496297345e4a
ekbaya <Eliasbaya1223@gmail.com>
2022-07-18 11:47:22 +0300

    Select By Year

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -110,7 +110,7 @@ class PaymentController extends Controller
         $payments = Payment::where([
             ['UserId', '=', $request->user()->user_id],
             ['Status', '=', 'paid'],
-        ])->selectRaw('year(created_at) year, monthname(created_at) month, count(*) payments')
+        ])->selectRaw('year(created_at) year, monthname(created_at) month, created_at')
         ->groupBy('year', 'month')
         ->orderBy('year', 'desc')
         ->get();
