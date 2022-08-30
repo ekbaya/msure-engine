83e5da53aeebf47062fcd44a210b104696153326
ekbaya <eliasbaya1223@gmail.com>
2022-08-30 11:32:38 +0300

    Gazzle

--- app/Services/EquityService.php
+++ app/Services/EquityService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
@@ -16,28 +18,34 @@ class EquityService
         $token = Cache::get($identifier . '_equity_payments_token');
 
         if (is_null($token)) {
+
+
+            $client = new Client();
+
+            $headers = [
+                'Content-Type' => 'application/x-www-form-urlencoded'
+            ];
             $payload = [
                 'form_params' => [
                     "client_secret" => config('app.equity.client_secret'),
                     "client_id" => config('app.equity.client_id'),
                     "grant_type" => config('app.equity.grant_type'),
-                ]];
+                ]
+            ];
 
-            Log::info('==GRANT-TYPE==' .config('app.equity.grant_type'));
+            $request = new Request('POST', config('app.equity.base_url') . '/v2.1/oauth/token', $headers);
+            $response = $client->sendAsync($request, $payload)->wait();
 
-            $response = Http::withHeaders([
-                'Content-Type' => 'application/x-www-form-urlencoded'
-            ])
-                ->withoutVerifying()
-                ->post(config('app.equity.base_url') . '/v2.1/oauth/token', $payload);
-            if ($response->successful()) {
-                Log::info('==EQUITY TOKEN==' . $response);
-                $token = $response->json('access_token');
-                Cache::put($identifier . '_equity_payments_token', $token, $response->json('expires_in') - 10);
-            } else {
-                Log::info('==ERROR==' . $response);
-                $response->throw();
-            }
+            Log::info('==EQUITY RESPONSE==' . $response);
+
+            // if ($response->successful()) {
+            //     Log::info('==EQUITY TOKEN==' . $response);
+            //     $token = $response->json('access_token');
+            //     Cache::put($identifier . '_equity_payments_token', $token, $response->json('expires_in') - 10);
+            // } else {
+            //     Log::info('==ERROR==' . $response);
+            //     $response->throw();
+            // }
         }
     }
 }
